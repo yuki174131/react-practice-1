aa871f8267ee8e9797195b095e67ab2e9deef985
yuki174131 <[yuki31.messi10@gmail.com]>
2022-11-12 23:22:24 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log('Change!')}}></input>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
   return (
     <React.Fragment>
-      <label htmlFor="bar">Bar</label>
-      <input type="text" onChange={() => {console.log("Change!!")}}/>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
-  )  
+  )
+}
+const Cat = () => {
+  return <div>Melow</div>
 }
 
 export default App;
